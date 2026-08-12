3a37b1f26c2c059474a493f355585509d1ac7bd4
Tanjim Kamal <tanjimkamal1@gmail.com>
2026-08-12 10:19:42 +0600

    add unicode support

--- index.js
+++ index.js
@@ -91,7 +91,12 @@ function getLanguageColor(language) {
 
 function decodeBase64(str) {
     try {
-        return atob(str.replace(/\s/g, ''));
+        const binary = atob(str.replace(/\s/g, ''));
+        const bytes = new Uint8Array(binary.length);
+        for (let i = 0; i < binary.length; i++) {
+            bytes[i] = binary.charCodeAt(i);
+        }
+        return new TextDecoder('utf-8').decode(bytes);
     } catch (e) {
         console.error('Base64 decode error:', e);
         return '';
